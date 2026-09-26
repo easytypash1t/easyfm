55244e775f7cbde64e1d06576eea9f9b5a60e0f8
Emre Kolay <easysp5der@gmail.com>
2026-09-26 23:03:34 +0300

    sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const STATIC_CACHE = 'sp1der-static-v40';
-const RUNTIME_CACHE = 'sp1der-runtime-v40';
+const STATIC_CACHE = 'sp1der-static-v41';
+const RUNTIME_CACHE = 'sp1der-runtime-v41';
 const APP_SHELL = [
   './',
   './index.html',
